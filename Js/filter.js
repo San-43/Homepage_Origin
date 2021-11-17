2dc0a024cc9a22f1b495ec021c75689448cb984c
Sandro Martínez Pérez <virgosmp.mx@gmail.com>
2021-11-16 23:19:11 -0600

    nice commit

---
+++ Js/filter.js
@@ -0,0 +1,60 @@
+$(function () {
+    $('.filter').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        let valor = $(this).attr('data-nombre');
+        if (valor == 'all') {
+            $('.cont-work').show('1000');
+        } else {
+            $('.cont-work').not('.' + valor).hide('1000');
+            $('.cont-work').filter('.' + valor).show('1000');
+        }
+    });
+
+    let team = $('#team').offset().top,
+        service = $('#services').offset().top,
+        work = $('#work').offset().top,
+        contact = $('#contact').offset().top;
+
+    window.addEventListener('resize', function () {
+        let team = $('#team').offset().top,
+            service = $('#service').offset().top,
+            work = $('#work').offset().top,
+            contact = $('#contact').offset().top;
+    });
+
+    $('#link-home').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 600);
+    });
+
+    $('#link-team').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: team - 100
+        }, 600);
+    });
+
+    $('#link-service').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: service - 100
+        }, 600);
+    });
+
+    $('#link-work').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: work - 100
+        }, 600);
+    });
+
+    $('#link-contact').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: contact - 100
+        }, 600);
+    });
+
+});
--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,69 @@
-window.onload = function() {
-    var contenedor = document.getElementById('onload');
+let nav = document.getElementById('nav');
+let menu = document.getElementById('links');
+let icon = document.getElementById('open');
+let buttons = document.getElementsByClassName('btn-header');
+let closed = true;
 
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = '0'; 
+function menus() {
+    let Desplazamiento_Actual = window.pageYOffset;
+
+    if (Desplazamiento_Actual <= 300) {
+        nav.classList.remove('nav2');
+        nav.className = ('nav1 navbar navbar-expand-sm fixed-top navbar-dark');
+        nav.style.transition = '1s';
+        menu.style.top = '80px';
+        icon.style.color = '#fff';
+    } else {
+        nav.classList.remove('nav1');
+        nav.className = ('nav2 navbar navbar-expand-sm fixed-top navbar-light');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+        icon.style.color = '#000';
+    }
 }
+
+function opening(){
+    if(closed){
+        menu.style.width = '30vw';
+        closed = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        closed = true;
+    }
+}
+
+window.addEventListener('scroll', function () {
+    console.log(window.pageYOffset);
+    menus();
+});
+
+window.addEventListener('load', function () {
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+
+window.addEventListener('click', function(e) {
+    if(closed == false) {
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== icon) {
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            closed = true;
+        }
+    }
+
+})
+
+window.addEventListener('resize', function() {
+    if(screen.width >=700) {
+        closed = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
+
+icon.addEventListener('click', function(){
+    opening();
+})
